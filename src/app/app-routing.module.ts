6c65466872a5832e30a97c3ff05891180f03aaa2
alex <alex@MacBook-Pro-de-alex.local>
2019-12-16 16:06:17 +0100

    generate pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'index', loadChildren: './index/index.module#IndexPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
+  { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' },
+  { path: 'chat', loadChildren: './chat/chat.module#ChatPageModule' },
+  { path: 'search', loadChildren: './search/search.module#SearchPageModule' }
 ];
 @NgModule({
   imports: [
