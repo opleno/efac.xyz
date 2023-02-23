63a18b72939f5aa5c5481df891890394ce7a2e49
Alex <opleno.dev@gmail.com>
2023-02-23 10:46:28 +0100

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{html,js,jsx}",
+    "./pages/**/*.{html,js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
