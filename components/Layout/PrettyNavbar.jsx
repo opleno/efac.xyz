d3c3a8e6fb20f68d357cdc262ff9bf5212d83f55
Alex <opleno.dev@gmail.com>
2023-03-28 13:31:05 +0200

    add PrettyNavbar

--- components/Layout/PrettyNavbar.jsx
+++ components/Layout/PrettyNavbar.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import Link from "next/link";
 
 const PrettyNavbar = () => {
   return (
-    <div>
-      hola
-    </div>
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex-shrink-0">
+            <Link href="/" className="text-white font-bold text-xl">
+              My Website
+            </Link>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <Link
+                href="/"
+                className="hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Home
+              </Link>
+              <Link
+                href="/about"
+                className="hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                About
+              </Link>
+              <Link
+                href="/blog"
+                className="hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Blog
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
